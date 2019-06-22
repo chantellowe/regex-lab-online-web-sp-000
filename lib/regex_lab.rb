b7b8bd3adf0f4a7c9ca1543fc3bfe8a53862dea6
Chantel Lowe <chantellowe17@gmail.com>
2019-06-22 03:41:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
 def starts_with_a_vowel?(word)
   if word.scan(/^[aeiouAEIOU]/)
     true
-  elsif !(word.scan(/^[aeiouAEIOU]/))
+  end
+  
+  if word.scan(/^[\w\W][^aeiouAEIOU]/)
     false
   end
 end
