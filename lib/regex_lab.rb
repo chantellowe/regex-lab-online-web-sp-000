08144095a27e767cf1b7310455f677116e58c087
Chantel Lowe <chantellowe17@gmail.com>
2019-06-22 03:30:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/^aeiouAEIOU/)
     true
-  elsif word.scan(/^b-dB-Df-hF-Hj-nJ-Np-tP-Tv-zV-Z/)
+  elsif word.match(/^b-dB-Df-hF-Hj-nJ-Np-tP-Tv-zV-Z/)
     false
   end
 end
